f5ffa2403b45931a96253687dd7227b51fac03f9
Kaleb Münch <104307796+KaMuench@users.noreply.github.com>
2023-06-15 12:28:05 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/Aufgabe_X.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Aufgabe_X.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/Aufgabe_X_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Aufgabe_X_autogen/mocs_compilation.cpp"
-  "CMakeFiles/Aufgabe_X_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Aufgabe_X_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
